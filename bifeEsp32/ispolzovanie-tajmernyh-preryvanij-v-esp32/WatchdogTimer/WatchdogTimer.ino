62e75518599649e85c7fcec2a2f62ed0a5c9f2a2
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-11-03 08:51:05 +0300

    WatchdogTimer.ino
     Пример прерывания и управления программным сторожевым таймером

--- bifeEsp32/ispolzovanie-tajmernyh-preryvanij-v-esp32/WatchdogTimer/WatchdogTimer.ino
+++ bifeEsp32/ispolzovanie-tajmernyh-preryvanij-v-esp32/WatchdogTimer/WatchdogTimer.ino
@@ -1,53 +1,68 @@
-/** Arduino-Esp32-CAM                             *** WatchdogTimer.ino ***
+/** Arduino-Esp32-CAM                                 *** WatchdogTimer.ino ***
  * 
- *                ----Базовый пример сторожевого таймера задач в ESP32 board 5.1.33
+ *              Пример прерывания и управления программным сторожевым таймером.
+ *                          
+ *    В скетче задается время сторожевому таймеру для перезагрузки контроллера, 
+ *   равное 3 секундам. Но пока контакт IO0 не замкнут на GND, скетч работает 1
+ *            секунду и сбрасывает сторожевой таймер, предотвращая перезагрузку 
+ *          контроллера. После замыкания контакта скетч переходит в бесконечный 
+ *    цикл, что вызывает прерывание через 3 секунды и перезагрузку контроллера.
  *                                        (на контроллере AI-Thinker ESP32-CAM)
  * 
- * v1.1, 12.10.2024                                   Автор:      Труфанов В.Е.
- * Copyright © 2024 tve                               Дата создания: 12.10.2024
- * 
- * Ключевой момент — установить задержку минимум в 1 мс после выполнения 
- * esp_task_wdt_reset:
+ * v1.1, 03.11.2024                                   Автор:      Труфанов В.Е.
+ * Copyright © 2024 tve                               Дата создания: 03.11.2024
 **/
 
-#include "esp_system.h"
-#include "rom/ets_sys.h"
+// #include "esp_system.h"
+// #include "rom/ets_sys.h"
 
-const int button = 0;         //gpio to use to trigger delay
-const int wdtTimeout = 3000;  //time in ms to trigger the watchdog
+// Назначаем 0 пин на остановку таймера
+const int button = 0;
+// Назначаем время срабатывания прерывания сторожевого таймера 3 секунды         
+const int wdtTimeout = 3000;  
+// Формируем заголовок таймера
 hw_timer_t *timer = NULL;
+// Определяем счетчик перезапусков главного цикла
+int icounter;
 
-void ARDUINO_ISR_ATTR resetModule() {
-  ets_printf("reboot\n");
-  esp_restart();
+void ARDUINO_ISR_ATTR resetModule() 
+{
+   ets_printf("\nПерезагрузка!\n");
+   esp_restart();
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("running setup");
-
-  pinMode(button, INPUT_PULLUP);                   //init control pin
-  timer = timerBegin(1000000);                     //timer 1Mhz resolution
-  timerAttachInterrupt(timer, &resetModule);       //attach callback
-  timerAlarm(timer, wdtTimeout * 1000, false, 0);  //set time in us
+void setup() 
+{
+   Serial.begin(115200);
+   Serial.println();
+   icounter = 1;
+   Serial.println("Запускаются настройки таймера в setup");
+   pinMode(button, INPUT_PULLUP);                   // init control pin
+   timer = timerBegin(1000000);                     // timer 1Mhz resolution
+   timerAttachInterrupt(timer, &resetModule);       // attach callback
+   timerAlarm(timer, wdtTimeout * 1000, false, 0);  // set time in us
 }
 
-void loop() {
-  Serial.println("running main loop");
+void loop() 
+{
+   ets_printf("Запускается главный цикл %d раз\n",icounter++);
+   // Фиксируем время начала цикла
+   long loopTime = millis();
+   // Выполняем основную работу цикла
+   delay(1000);  
+   loopTime = millis() - loopTime;
+   ets_printf("Отработано в цикле %d миллисекунд.\n",loopTime);
+   // Сбрасываем WatchdogTimer - "Кормим сторожевого пса"
+   timerWrite(timer, 0); 
+   Serial.println(" ");
 
-  timerWrite(timer, 0);  //reset timer (feed watchdog)
-  long loopTime = millis();
-  //while button is pressed, delay up to 3 seconds to trigger the timer
-  while (!digitalRead(button)) {
-    Serial.println("button pressed");
+   // Пока IO0 замкнут на GND, с интервалом в полсекунды выводим сообщение
+   // до тех пор, пока не произойдет перезагрузка контроллера
+  while (!digitalRead(button)) 
+  {
+    Serial.println("IO0 замкнут на GND");
     delay(500);
   }
-  delay(1000);  //simulate work
-  loopTime = millis() - loopTime;
-
-  Serial.print("loop time is = ");
-  Serial.println(loopTime);  //should be under 3000
 }
 
-// ************************************************** ex5-1-33_BasicWDT.ino ***
+// ****************************************************** WatchdogTimer.ino ***
